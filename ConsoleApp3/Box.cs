02b72ab23244bcbdfd77a98ae6fa11913261acc8
ani.s.avetisyan <ani.s.avetisyan@amx.am>
2023-07-03 16:05:33 +0400

    Add project files.

---
+++ ConsoleApp3/Box.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Box
+    {
+        private int[] arr;
+        
+        public int len;
+        public Box() { }
+        public Box(int[] x)
+        {
+            arr = x;
+        }
+
+        public void SetLength(int arrLength)
+        {
+            len = arrLength;
+        }
+
+
+        public void ShowArray()
+        {
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+        public static Box operator +(Box b1, Box b2)
+        {
+            int maxLen = b1.len > b2.len ? b1.len : b2.len;
+            Array.Resize<int>(ref b1.arr, maxLen);
+            Array.Resize<int>(ref b2.arr, maxLen);
+            if (b1.len > b2.len)
+            {
+
+            }
+            Box box = new Box();
+            box.arr = new int[maxLen];
+
+            for (int i = 0; i < maxLen; i++)
+            {
+                box.arr[i] = b1.arr[i] + b2.arr[i];
+            }
+            return box;
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp3;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arr1 = { 1, 2, 3, 4 };
+        int[] arr2 = { 2, 3, 4, 5, 4};
+
+        Box box1 = new Box(arr1);
+        box1.SetLength(arr1.Length);
+        
+
+        Box box2 = new Box(arr2);
+        box2.SetLength(arr2.Length);
+
+        Box box3 = new Box();
+        box3 = box1 + box2;
+        box3.ShowArray();
+        Console.ReadKey();
+
+    }
+}
